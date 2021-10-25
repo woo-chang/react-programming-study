60e07e5565d41d85afa56922cb69fd2065f7432e
choiwoochang <clllickme@naver.com>
2021-10-25 23:28:24 +0900

    디바운스 예제_1

---
+++ do-it-example/src/02/debounce.js
@@ -0,0 +1,19 @@
+// 디바운스
+// 어떤 내용을 입력하다가 특정 시간 동안 대기하고 있으면 마지막에 입력된 내용을 바탕으로 서버 요청을 하는 방법
+function debounce(func, delay) {
+    let inDebounce;
+    return function(...args) {
+        if (inDebounce) {
+            clearTimeout(inDebounce);
+        }
+        inDebounce = setTimeout(
+            () => func(...args),
+            delay
+        );
+    }
+}
+
+const run = debounce(val => console.log(val), 100);
+run('a')
+run('b')
+run(2)
